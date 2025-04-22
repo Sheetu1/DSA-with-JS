7cceb5d92bf24f39b50d95646f6e755cbea30d82
Sheetu1 <sheetusharma548@gmail.com>
2025-04-22 23:32:30 +0530

    Pallindrome something changes

--- strings/script.js
+++ strings/script.js
@@ -1,15 +1,15 @@
 //  let name = "sheetal";
 //  console.log(name);
 // console.log(name[1]);
-// name = name + "sharma";  direct change nhi kr skte immutable hoti h string but reassign krke valaue me changes kr skte h 
+// name = name + "sharma";  direct change nhi kr skte immutable hoti h string but reassign krke valaue me changes kr skte h
 //  console.log(name);
 
 // console.log(name.length);  length property h without bracess use krte h baki sb methods h js me
 // console.log(name.toUpperCase());
 // console.log(name.toLowerCase());
-// console.log(name.slice(1,5)); starting index to as a tis aa jaati h lekin last me ek extra dena pdta h ise positive or negative dono ke liye use krte h 
+// console.log(name.slice(1,5)); starting index to as a tis aa jaati h lekin last me ek extra dena pdta h ise positive or negative dono ke liye use krte h
 // console.log(name.slice(-6,name.length)); negative me
-// console.log(name.substring(1,5)); starting andd ending part ka  new string deti h dono slice and substring bs slice positive negetive dono ke liye or substring only postive values ke liye use krte hai 
+// console.log(name.substring(1,5)); starting andd ending part ka  new string deti h dono slice and substring bs slice positive negetive dono ke liye or substring only postive values ke liye use krte hai
 // console.log(name.substring(1)); and substring me last index nhi denge to to vah last tk pick kr lega lekin slice me dono dene pdte h
 // console.log(name.concat(" " + "Sharma"));
 // let name =    "sheetal      "
@@ -28,7 +28,7 @@
 // qs2 print in reverse order
 // let name = "Sheetal";
 // for(let i=name.length-1; i>=0; i--){
-//     console.log(name.charAt(i));  
+//     console.log(name.charAt(i));
 // }
 // qs2 print in reverse order "lateehs"
 // let name = "Sheetal";
@@ -38,24 +38,23 @@
 // }
 // console.log(rev);
 
-
 // qs3 check if string is pallindrom or not(forward and backward same read that is pallindrom)
-// let str = prompt("Enter any string")
-// let isPallindrome = true;
-// let i=0, j=str.length-1;
-// while(i<j){
-//     if(str.charAt(i) != str.charAt(j))
-//     {
-//         isPallindrome = false;
-//         break;
-//     }
-//     i++;
-//     j--;
-// }
-
-// if(isPallindrome) console.log('Pallindrome');
-// else console.log('Not Pallindrome');
-
+let str = prompt("Enter any string");
+let isPallindrome = true;
+let i=0, j=str.length-1;
+while (i < j) {
+  if (str.charAt(i) != str.charAt(j)) {
+    isPallindrome = false;
+    break;
+  }
+  i++;
+  j--;
+}
+if (isPallindrome) {
+  console.log("Pallindrome");
+} else {
+  console.log("Not Pallindrome");
+}
 
 // qs4 toggle the given string
 // let str = prompt("Enter any String");
@@ -70,26 +69,27 @@
 //     else if(ch >= 97 && ch <= 122) {
 //         toggle = toggle + String.fromCharCode(ch - 32);  // Lower to upper
 //     }
-  
+
 // }
 
 // console.log(toggle);
 
+// qs-5 Frequency of each characters
+//  ex-> hello
+// h-1
+// e-1
+// l-2
+// o-1
+// konsa charcter kitni bar aya h
 
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
- 
+// let str = prompt("Enter any String");
+// let arr = new Array(128).fill(0);
+// for(let i = 0; i< str.length-1; i++){
+//     let idx = str.charCodeAt(i);
+//     arr[idx] = arr[idx] + 1;
+// }
+// for(let i = 0; i< str.length-1; i++){
+//     if(arr[i]>0){
+//         console.log(String.fromCharCodeAt(i) + "Appears at" + arr[i] + "times");
+//     }
+// }
