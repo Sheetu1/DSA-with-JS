e3d4ce31556bd394ba17a6000db82eab3806b197
Sheetu1 <sheetusharma548@gmail.com>
2025-04-25 23:54:52 +0530

    Second min

--- array/script.js
+++ array/script.js
@@ -14,14 +14,13 @@
 // arr[5] = 50;
 // console.log(arr);
 
-
 // let arr = new Array(5);
 // for(let i=0; i<arr.length; i++){
 //     arr[i] = Number(prompt("Enter a number"))
 // }
 // console.log(arr);
 
-// qs1  sum of n elements in an array 
+// qs1  sum of n elements in an array
 // let arr = [10,20,30,40,100]
 // sum = 0;
 // for(let i=0; i<arr.length; i++){
@@ -29,10 +28,9 @@
 // }
 // console.log(sum);
 
-
 // let arr = new Array(5);
 // for(let i=0; i<arr.length; i++){
-//     arr[i] = Number(prompt('Enter any number'));  
+//     arr[i] = Number(prompt('Enter any number'));
 // }
 // let sum = 0;
 // for(let i=0; i<arr.length; i++){
@@ -40,10 +38,7 @@
 // }
 // console.log(sum);
 
-
-
-
-// qs2  maximum number of an element 
+// qs2  maximum number of an element
 
 // let arr = [10,20,35,12,180,245]
 // let max = arr[0]
@@ -54,8 +49,7 @@
 // }
 // console.log(max);
 
-
-// qs3  minimum number of an element 
+// qs3  minimum number of an element
 
 // let arr = [10,20,35,12,180,245,5]
 // let min = arr[0]
@@ -66,8 +60,7 @@
 // }
 // console.log(min);
 
-
-// qs4 2nd maximum number 
+// qs4 2nd maximum number
 
 // let arr = [10,20,50,68,22,44]
 // let max = Math.max(arr[0],arr[1])
@@ -84,25 +77,23 @@
 // }
 // console.log(smax);
 
+// qs5 2nd minimum number
+let arr = [1, 20, 50, 68, 88, 74, 12, 5];
 
-// qs5 2nd minimum number 
-// let arr = [1, 20, 50, 68, 88, 74, 12, 5];
+let min = Infinity;
+let smin = Infinity;
 
-// let min = Infinity;
-// let smin = Infinity;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] < min) {
+    smin = min;
+    min = arr[i];
+  } else if (arr[i] > min && arr[i] < smin) {
+    smin = arr[i];
+  }
+}
+console.log("Second minimum:", smin);
 
-// for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] < min) {  
-//         smin = min;  
-//         min = arr[i];  
-//     } else if (arr[i] > min && arr[i] < smin) {  
-//         smin = arr[i];  
-//     }
-// }
-//     console.log("Second minimum:", smin);
-
-
-// qs6 reverse an array 
+// qs6 reverse an array
 // let arr = [10,20,30,40,50]
 // let i=0, j=arr.length-1;
 // while(i!=j){
@@ -114,50 +105,23 @@
 // }
 // console.log(arr);
 
-
-
-
 // qs7 two pointer based zero values and 1 values
 
-let arr = [1,1,0,1,0,1,1,0,0]
-let i =0 ,j =0;
-while(i<arr.length){
-    if(arr[i]==0){
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-        j++;
-    }
-    i++;
-}
+// let arr = [1,1,0,1,0,1,1,0,0]
+// let i =0 ,j =0;
+// while(i<arr.length){
+//     if(arr[i]==0){
+//         let temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//         j++;
+//     }
+//     i++;
+// }
 // console.log(arr);
 
 // qs8 move the all negative element left side and positive element move the right side
 
 // let arr = [-1,2,3,4,-5,-6,7,8,,9,-10]
 
-// qs-9 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// qs-9
