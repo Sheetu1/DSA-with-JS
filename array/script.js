33234c1d27a083b1a112353e2c3d71296c215448
Sheetu1 <sheetusharma548@gmail.com>
2025-04-26 00:42:58 +0530

    Reverse array without extra space

--- array/script.js
+++ array/script.js
@@ -124,14 +124,30 @@
 
 // let arr = [-1,2,3,4,-5,-6,7,8,,9,-10]
 
-// qs-9 reverse array 
+// qs-9 reverse array  with extra space
+
+// let arr = [10,20,30,40,50]
+// let temp = new Array(arr.length);
+// let j=0;
+// for(let i=arr.length-1; i>=0; i--){
+//     temp[j] = arr[i];
+//     j++;
+// }
+// console.log(temp);
+
+// qs-10 without extra space
 
 let arr = [10,20,30,40,50]
-let temp = new Array(arr.length);
-let j=0;
-for(let i=arr.length-1; i>=0; i--){
-    temp[j] = arr[i];
-    j++;
+let i =0, j = arr.length-1;
+let temp;
+while(i!=j){
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+    i++
+    j--
 }
-console.log(temp);
+console.log(arr);
+
+
 
